e64b32ee1be46d938e3007bfdf10e2318b26c2e6
ParsaNaderiX <parsanaderi2004@gmail.com>
2025-09-30 17:43:46 +0330

    Implement book management endpoints

--- app/main.py
+++ app/main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
 
+books = {}
+
 app = FastAPI()
 
 @app.get("/health")
 def health() -> dict:
-    return {"status": "ok"}
+    return {"status": "OK"}
+
+@app.post("/add-book")
+def add_book(book_id: int, book_title: str, book_author: str,
+                book_year: int, book_description: str | None = None) -> dict:
+    if book_id in books:
+        return {"Error": "We already have the book!"}
+    
+    books[book_id] = {"title": book_title, "author": book_author,
+                        "year": book_year, "description": book_description}
+    return {"status": "OK"}
+
+@app.get("/get-book/{book_id}")
+def get_book(book_id: int) -> dict:
+    if book_id not in books:
+        return {"Error": "We don't have the book!"}
+    
+    return books[book_id]
